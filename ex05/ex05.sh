9e4761a3a337463c537ae77c57dc639f76089ee7
Alexander <sashaersh27@gmail.com>
2018-06-12 17:14:12 +0300

    ex05 completed

--- ex05/ex05.sh
+++ ex05/ex05.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+
 if [ -z $2 ]; then
 	echo "0"
 	exit 0
-
-if ! [[ $2 =~ [[:digit:]] ]]; then
-     echo "Error.."
-     exit 1
 fi
 
-flag=$1
-count=0
-sum=0
+summ=0
 
-case $flag in
+for i in $@
+do
+  if [ "$1" == "-s" ]; then
+    summ=$(echo "$summ + $i" | bc)
+  elif [ "$1" == "-m" ]; then
+    summ=$(echo "$summ + $i" | bc)
+  elif [ "$1" == "-e" ]; then
+    [ $((i%2)) -eq 0 ] && summ=$(echo "$summ + $i" | bc)
+  elif [ "$1" == "-o" ]; then
+    [ $((i%2)) -ne 0 ] && summ=$(echo "$summ + $i" | bc)
+  else
+    echo "Error.."
+    exit 1
+  fi
+done
+
+if [ "$1" == "-m" ]; then
+  shift 1
+  summ=$(echo $(( "$summ / $#" )))
+fi
+
+echo "$summ"
