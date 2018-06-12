7d14fc29afe698c15a84b7d46856209d7b740146
Alexander <sashaersh27@gmail.com>
2018-06-12 15:50:28 +0300

    ex09 completed

--- ex09/ex09.sh
+++ ex09/ex09.sh
@@ -4,6 +4,10 @@ if [ -z $1 ] || [ -z $2 ]; then
   exit 1
 fi
 
-if [ $1 -eq "-u" | "--url" ]; then
-  grep -Eo "https:\/\/([[:alnum:]]|\.|\/)+" $2
+if [ "$1" = "-u" ] || [ "$1" = "--url" ]; then
+  grep -Po '(^|\t)https:[A-Za-z0-9\/\.]+' $2
+elif [ "$1" = "-e" ] || [ "$1" = "--email" ]; then
+  grep -Po '(^|\t)[A-Za-z0-9\.]+@[A-Za-z0-9\.]+\.[A-Za-z0-9\.]+' $2
+else
+  exit 1
 fi
